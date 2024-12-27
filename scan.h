b3b4fab4e033510005d6c926c6cefad3ffd8cdec
rattus-digitalis <victor.michi@coda-student.school>
2024-12-27 18:46:08 +0100

    Update scan.h

--- scan.h
+++ scan.h
@@ -1,10 +1,8 @@
 #ifndef SCAN_H
 #define SCAN_H
 
-// Déclaration de la fonction pour exécuter une commande (nmap, etc.)
-void execute_command(const char *title, const char *command);
 
-// Déclaration de la fonction de scan dans un thread
+void execute_command(const char *title, const char *command);
 void *perform_scan(void *args);
 
 #endif // SCAN_H
